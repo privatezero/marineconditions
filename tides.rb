782c13ca1aea68a8e264e52afae2a08ed4ef211d
Andrew Weaver <theandrewjw@gmail.com>
2024-01-14 23:53:43 -0800

    add west point current

--- tides.rb
+++ tides.rb
@@ -5,5 +5,6 @@ url = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&stat
 uri = URI(url)
 response = Net::HTTP.get(uri)
 JSON.parse(response)
+# Current speed at West Point - https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=PUG1515_10&product=currents_predictions&time_zone=lst&interval=MAX_SLACK&units=english&application=DataAPI_Sample&format=xml&bin=1
 # NOAA Tides API info - https://api.tidesandcurrents.noaa.gov/api/prod/
 # Maybe try this gem for visuals https://www.ruby2d.com/
