57389895833eee659afd1f7db6e724784789484f
Andrew Weaver <theandrewjw@gmail.com>
2024-01-14 22:49:02 -0800

    initial tides

---
+++ tides.rb
@@ -0,0 +1,7 @@
+require 'net/http'
+require 'json'
+
+url = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=today&station=9447130&product=predictions&datum=MLLW&time_zone=lst&interval=hilo&units=english&application=DataAPI_Sample&format=json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+JSON.parse(response)
